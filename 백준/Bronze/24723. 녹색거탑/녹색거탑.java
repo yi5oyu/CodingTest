4b4e27b4559d8b0fb75881938685323754b21d11
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 01:03:37 +0900

    [Bronze IV] Title: 녹색거탑, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] aa) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int answer = 1;
+        for(int i = 0;i<num ;i++)
+            answer*=2;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(""+answer);
+        bw.close();
+    }
+}
