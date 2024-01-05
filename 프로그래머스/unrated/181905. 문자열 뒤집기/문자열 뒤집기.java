a3018972a7da4c3de2c888c8f90b5c4477ba4a5b
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 20:43:47 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 12.44 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        for(int i = 0 ; i<my_string.length() ;i++){
+            if(i < s || i>e){
+                answer += "" + my_string.charAt(i);
+            } else if(i == e){
+                for(int j = e ; j>=s ; j--){
+                    answer += "" + my_string.charAt(j);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
