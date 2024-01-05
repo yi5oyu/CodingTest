485dc986195b230941e14bc7806a086c11f8f30f
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 20:39:40 +0900

    [level unrated] Title: 수 조작하기 2, Time: 641.44 ms, Memory: 372 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        int inputnum = 0;
+        for(int i = 1 ;i<numLog.length ;i++){
+            inputnum = numLog[i] - numLog[i-1];
+            switch (inputnum){
+                case 1:
+                    answer += "w";
+                    break;
+                case -1:
+                    answer += "s";
+                    break;
+                case 10:
+                    answer += "d";
+                    break;
+                case -10:
+                    answer += "a";
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
