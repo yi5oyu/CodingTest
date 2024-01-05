06fa2d6f8f9f498f26514b530537823b3e202a6a
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 20:54:44 +0900

    [level unrated] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = -1 ;
+        int index = 0; 
+        for(int i = 1 ;i<array.length ;i++){
+            if(max < array[i]){
+                max = array[i];
+                index = i;
+            }
+        }
+        answer[0] = max;
+        answer[1] = index;
+        return answer;
+    }
+}
