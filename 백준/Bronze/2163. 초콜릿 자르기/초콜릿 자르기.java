df6b864dae359d7d51d63cf0b12179171185aa15
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 00:37:01 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] aa) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(""+((a-1)+a*(b-1)));
+        bw.close();
+    }
+}
