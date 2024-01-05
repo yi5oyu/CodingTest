48f2a1c59d8e3db57673e886e4b08efcd6452ac8
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 01:05:25 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6112 ms, Memory: 408524 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] aa) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger bigIntA = new BigInteger(st.nextToken());
+        BigInteger bigIntB = new BigInteger(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(""+bigIntA.multiply(bigIntB));
+        bw.close();
+    }
+}
