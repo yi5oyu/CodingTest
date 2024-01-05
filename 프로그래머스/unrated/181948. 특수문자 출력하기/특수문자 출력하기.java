c604871131d85ef5ed5da19dd1ec57adb969d0b9
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 20:33:27 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 164.22 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //Scanner scan = new Scanner(System.in);
+        String a = "!@#$%^&*(";
+        a +=  "" + (char)92 + "'" + '"';
+        a +=  "<>?:;";
+        
+        System.out.print(a);
+        
+    }
+}
