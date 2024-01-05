db4721e4968ca23eae00973c455db8a07ba543c9
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 21:07:59 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int min = 0;
+        int max = 100;
+        int answer = 0;
+        if(min < num1 && num1 <= max && min < num1 && num1 <= max ){
+            answer = num1 % num2;
+        }
+        return answer;
+    }
+}
