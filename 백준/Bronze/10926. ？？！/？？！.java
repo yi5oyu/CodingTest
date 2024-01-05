f776f13963f5a5d3de422a6cd5955a9cbad12bf2
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 01:09:31 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]aa){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.print(a+"??!");
+    }
+}
