2aef2737bd2ad494955df40b268fe067bd0f61b9
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 01:12:35 +0900

    [Bronze V] Title: 구구단, Time: 248 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]aa){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i = 1 ;i<10 ;i++){
+            System.out.println(a+" * "+i+" = "+i*a);
+        }
+    }
+}
