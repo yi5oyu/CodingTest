f2bf0db8e50be51659e4e7c7d62ab92b5446367a
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 01:05:48 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]aaa){
+        Scanner sc = new Scanner(System.in);
+        Long a = sc.nextLong();
+        Long b = sc.nextLong();
+        Long c = sc.nextLong();
+        System.out.print(a+b+c);
+    }
+}
