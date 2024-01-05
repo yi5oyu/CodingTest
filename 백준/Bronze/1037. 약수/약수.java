d1c216b1b23bf7b11439084e29efd5bee8a3ef53
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 00:38:10 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] aa) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = -1;
+        int min = 1000001;
+        for(int i = 0 ; i<len ;i++){
+            int a = Integer.parseInt(st.nextToken());
+            if(a > max)
+                max =a;
+            if(a < min)
+                min =a;
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(""+(max*min));
+        bw.close();
+    }
+}
