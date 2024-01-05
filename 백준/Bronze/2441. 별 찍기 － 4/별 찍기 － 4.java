4754fca3dc80ce0ba0609c19b98f543da8ee0bcc
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-06 00:48:39 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] aa) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0 ; i<len;i++){
+            for(int j =0; j<i;j++)
+                bw.write(" ");
+            for(int j = len;j>i;j--)
+                bw.write("*");
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
