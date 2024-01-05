b8edb7840bea79846194ecaad361d789d07c4900
yi5oyu <111046436+yi5oyu@users.noreply.github.com>
2024-01-05 21:07:18 +0900

    [level unrated] Title: 두 수의 합, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int min = -50000;
+        int max = 50000;
+        int answer = -1;
+        if(min <= num1 && num1 <= max && min <= num1 && num1 <= max ){
+            answer = num1 + num2;
+        }
+        return answer;
+    }
+}
