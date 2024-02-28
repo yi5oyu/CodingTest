f2704a986ef35f6b6cf197752366db7d79118b27
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-02-28 19:44:47 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14484 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,20 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 
 public class Main{
     public static void main(String[] aa) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int len = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        ArrayList<Integer> intList = new ArrayList<Integer>();
-        for(int i = 0 ;i<len ;i++)
-            intList.add(str.charAt(i)-96);
-        int sum = 0;
-        for(int i = 0 ; i<len ;i++)
-            sum+= intList.get(i)*Math.pow(31,i);
+        BigInteger answer = new BigInteger("0");
+        BigInteger r = new BigInteger("31");
+        for(int i = 0 ; i<str.length() ;i++){
+            BigInteger x = new BigInteger(""+(str.charAt(i)-96));
+            answer = answer.add(x.multiply(r.pow(i)));
+        }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(""+sum);
+        bw.write(""+answer.remainder(new BigInteger("1234567891")));
         bw.close();
     }
 }
